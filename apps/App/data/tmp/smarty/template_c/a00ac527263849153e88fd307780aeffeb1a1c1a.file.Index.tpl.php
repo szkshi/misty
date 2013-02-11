0f3f50360b163dbcc89932d23940f2ea5d0030ad
Shuhei Suzuki <szk.shi@gmail.com>
2013-02-12 02:01:12 +0900

    rm:.git* in vendor

---
+++ apps/App/data/tmp/smarty/template_c/a00ac527263849153e88fd307780aeffeb1a1c1a.file.Index.tpl.php
@@ -0,0 +1,43 @@
+<?php /* Smarty version Smarty-3.1-DEV, created on 2013-02-12 01:49:16
+         compiled from "/Users/shuhei/Documents/web/github/misty/apps/App/Resource/Page/Index.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:13089283145119210cca99e5-78581172%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'a00ac527263849153e88fd307780aeffeb1a1c1a' => 
+    array (
+      0 => '/Users/shuhei/Documents/web/github/misty/apps/App/Resource/Page/Index.tpl',
+      1 => 1360600701,
+      2 => 'file',
+    ),
+    'f0fc41631227375eefd15d027bfc6dad819bcceb' => 
+    array (
+      0 => '/Users/shuhei/Documents/web/github/misty/apps/App/Resource/View/layout/default.tpl',
+      1 => 1360600701,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '13089283145119210cca99e5-78581172',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1-DEV',
+  'unifunc' => 'content_5119210cd07c58_66465449',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_5119210cd07c58_66465449')) {function content_5119210cd07c58_66465449($_smarty_tpl) {?><!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Index</title>
+    <link href="/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="/assets/css/bootstrap-responsive.css" rel="stylesheet">
+    <script src="/assets/js/jquery.js"></script>
+</head>
+<body>
+    <div class="container">
+    <h1><?php echo $_smarty_tpl->tpl_vars['greeting']->value;?>
+</h1>
+</div>
+</body>
+</html><?php }} ?>
